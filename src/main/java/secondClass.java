9a91016957a8e0ad39b06b97b0c6f33f837cd379
dbwlgns777 <yoojihoon777@daum.net>
2026-01-06 16:04:38 +0900

    before pull clasdd update test

--- src/main/java/secondClass.java
+++ src/main/java/secondClass.java
@@ -3,14 +3,5 @@ public class secondClass
     public void testFunction()
     {
         String test = "test";
-        if(test.equals("test"))
-        {
-            System.out.println("test!! Function In =>");
-        }
-
-        for (int i = 0; i < 5; i++)
-        {
-            System.out.println(" i Value => " + i);
-        }
     }
 }
