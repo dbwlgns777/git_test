feccfe7c3129ddaa6a74789426541fe915882c40
dbwlgns777 <yoojihoon777@daum.net>
2026-01-06 11:33:52 +0900

    second class add

---
+++ src/main/java/secondClass.java
@@ -0,0 +1,16 @@
+public class secondClass
+{
+    public void testFunction()
+    {
+        String test = "test";
+        if(test.equals("test"))
+        {
+            System.out.println("test!! Function In =>");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.println(" i Value => " + i);
+        }
+    }
+}
