777d317184eb1b04e11fd2c2941293f882e7c475
dbwlgns777 <yoojihoon777@daum.net>
2026-01-06 10:12:33 +0900

    second debug add

--- src/main/java/mainClass.java
+++ src/main/java/mainClass.java
@@ -4,5 +4,6 @@ public class mainClass
     {
         System.out.println("FF@@@@@@@@@@@@@");
         System.out.println("1111111111111111");
+        System.out.println("2222222222222222");
     }
 }
